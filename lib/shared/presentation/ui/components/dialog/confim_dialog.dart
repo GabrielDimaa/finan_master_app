285e369d410151cfa31824be3386a293fad9f4f5
Gabriel de Matos <gabrielmatos1999@rede.ulbra.br>
2023-08-09 23:50:19 -0300

    Criado dialog de confirmação

---
+++ lib/shared/presentation/ui/components/dialog/confim_dialog.dart
@@ -0,0 +1,44 @@
+import 'package:finan_master_app/shared/presentation/mixins/theme_context.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ConfirmDialog extends StatefulWidget {
+  final String? title;
+  final String message;
+
+  const ConfirmDialog({Key? key, this.title, required this.message}) : super(key: key);
+
+  static Future<bool> show({required BuildContext context, String? title, required String message}) async {
+    return (await showDialog<bool>(
+          context: context,
+          builder: (_) => ConfirmDialog(title: title, message: message),
+        )) ??
+        false;
+  }
+
+  @override
+  State<ConfirmDialog> createState() => _ConfirmDialogState();
+}
+
+class _ConfirmDialogState extends State<ConfirmDialog> with ThemeContext {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: widget.title != null ? Text(widget.title ?? '') : null,
+      content: Padding(
+        padding: widget.title != null ? EdgeInsets.zero : const EdgeInsets.only(top: 16),
+        child: Text(widget.message),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => context.pop(false),
+          child: Text(strings.cancel),
+        ),
+        TextButton(
+          onPressed: () => context.pop(true),
+          child: Text(strings.confirm),
+        ),
+      ],
+    );
+  }
+}
