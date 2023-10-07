37587ffa249def9a55ff754c12b993da0cd9eda2
Gabriel de Matos <gabrielmatos1999@rede.ulbra.br>
2023-10-07 13:16:40 -0300

    Adicionado bandeiras de cartão

---
+++ lib/features/credit_card/domain/enums/brand_card_enum.dart
@@ -0,0 +1,64 @@
+import 'package:collection/collection.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+enum CardBrandEnum {
+  alelo(1),
+  americanExpress(2),
+  banescard(3),
+  banricompras(4),
+  cabal(5),
+  dinersClub(6),
+  elo(7),
+  goodcard(8),
+  hipercard(9),
+  jcb(10),
+  mastercard(11),
+  sorocred(12),
+  visa(13);
+
+  final int value;
+
+  const CardBrandEnum(this.value);
+
+  static CardBrandEnum? getByValue(int value) => CardBrandEnum.values.firstWhereOrNull((v) => v.value == value);
+}
+
+extension CardBrandExtension on CardBrandEnum {
+  String get description => switch (this) {
+    CardBrandEnum.alelo => 'Alelo',
+    CardBrandEnum.americanExpress => 'American Express',
+    CardBrandEnum.banescard => 'Banescard',
+    CardBrandEnum.banricompras => 'Banricompras',
+    CardBrandEnum.cabal => 'Cabal',
+    CardBrandEnum.dinersClub => 'Diners Club',
+    CardBrandEnum.elo => 'Elo',
+    CardBrandEnum.goodcard => 'Good Card',
+    CardBrandEnum.hipercard => 'Hipercard',
+    CardBrandEnum.jcb => 'JCB',
+    CardBrandEnum.mastercard => 'Mastercard',
+    CardBrandEnum.sorocred => 'Sorocred',
+    CardBrandEnum.visa => 'Visa',
+  };
+
+  Widget icon({double size = 36, Color? color}) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100),
+      child: switch (this) {
+        CardBrandEnum.alelo => SvgPicture.asset('assets/icons/card-brands/alelo.svg', height: size, width: size),
+        CardBrandEnum.americanExpress => SvgPicture.asset('assets/icons/card-brands/americanexpress.svg', height: size, width: size),
+        CardBrandEnum.banescard => SvgPicture.asset('assets/icons/card-brands/banescard.svg', height: size, width: size),
+        CardBrandEnum.banricompras => SvgPicture.asset('assets/icons/card-brands/banricompras.svg', height: size, width: size),
+        CardBrandEnum.cabal => SvgPicture.asset('assets/icons/card-brands/cabal.svg', height: size, width: size),
+        CardBrandEnum.dinersClub => SvgPicture.asset('assets/icons/card-brands/dinersclub.svg', height: size, width: size),
+        CardBrandEnum.elo => SvgPicture.asset('assets/icons/card-brands/elo.svg', height: size, width: size),
+        CardBrandEnum.goodcard => SvgPicture.asset('assets/icons/card-brands/goodcard.svg', height: size, width: size),
+        CardBrandEnum.hipercard => SvgPicture.asset('assets/icons/card-brands/hipercard.svg', height: size, width: size),
+        CardBrandEnum.jcb => SvgPicture.asset('assets/icons/card-brands/jcb.svg', height: size, width: size),
+        CardBrandEnum.mastercard => SvgPicture.asset('assets/icons/card-brands/mastercard.svg', height: size, width: size),
+        CardBrandEnum.sorocred => SvgPicture.asset('assets/icons/card-brands/sorocred.svg', height: size, width: size),
+        CardBrandEnum.visa => SvgPicture.asset('assets/icons/card-brands/visa.svg', height: size, width: size),
+      },
+    );
+  }
+}
