a754308c69fafa34d791f7d3bbd4b6394b6d852a
Gabriel de Matos <gabrielmatos1999@rede.ulbra.br>
2024-11-16 01:47:13 -0300

    Correção ao abrir relatórios

--- lib/features/reports/infra/repositories/report_categories_repository.dart
+++ lib/features/reports/infra/repositories/report_categories_repository.dart
@@ -5,7 +5,9 @@ import 'package:finan_master_app/features/category/infra/data_sources/i_category
 import 'package:finan_master_app/features/category/infra/models/category_model.dart';
 import 'package:finan_master_app/features/reports/domain/entities/report_category_entity.dart';
 import 'package:finan_master_app/features/reports/domain/repositories/i_report_categories_repository.dart';
+import 'package:finan_master_app/features/transactions/domain/entities/expense_entity.dart';
 import 'package:finan_master_app/features/transactions/domain/entities/i_transaction_entity.dart';
+import 'package:finan_master_app/features/transactions/domain/entities/income_entity.dart';
 import 'package:finan_master_app/features/transactions/helpers/factories/expense_factory.dart';
 import 'package:finan_master_app/features/transactions/helpers/factories/income_factory.dart';
 import 'package:finan_master_app/features/transactions/infra/data_sources/i_expense_local_data_source.dart';
@@ -52,13 +54,13 @@ class ReportCategoriesRepository implements IReportCategoriesRepository {
 
     final List<CategoryModel> categories = await _categoriesLocalDataSource.findAll(
       where: 'id IN (${transactions.map((_) => '?').join(', ')})',
-      whereArgs: transactions.map((e) => e is ExpenseModel ? (e as ExpenseModel).idCategory : (e as IncomeModel).idCategory).toList(),
+      whereArgs: transactions.map((e) => e is ExpenseEntity ? e.idCategory : (e as IncomeEntity).idCategory).toList(),
       deleted: true,
     );
 
     final List<ReportCategoryEntity> result = [];
 
-    final Map<String, List<ITransactionEntity>> group = groupBy(transactions, (e) => e is ExpenseModel ? (e as ExpenseModel).idCategory : (e as IncomeModel).idCategory);
+    final Map<String, List<ITransactionEntity>> group = groupBy(transactions, (e) => e is ExpenseEntity ? e.idCategory! : (e as IncomeEntity).idCategory!);
 
     group.forEach((key, value) {
       result.add(ReportCategoryEntity(category: CategoryFactory.toEntity(categories.firstWhere((e) => e.id == key)), transactions: transactions));
